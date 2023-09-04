5ec010fddcc5b580f1b710f4288833f7914290cc
Chawannua <chawannua@gmail.com>
2023-09-04 19:19:25 +0700

    Line bot creation

---
+++ line bot.py
@@ -0,0 +1,47 @@
+import requests
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
+# Replace with your own values
+LINE_CHANNEL_SECRET = '22550507fb118de47cd3d0fa8153bbe6'
+LINE_CHANNEL_ACCESS_TOKEN = '7nntV9CadnWw54gO9B+lAJTF1Ap4RF5lCJatqOLRrzHZO0wrSewxnSh8bV9kJSHf0xuwIPW5gw+08gH3W3nVK6KuDW9AB6ctP5SxleybdphHk4klApt8z68dp2OXcliJ27pXppy4Un4cx7j8DTXraAdB04t89/1O/w1cDnyilFU='
+
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(LINE_CHANNEL_SECRET)
+
+app = Flask(__name__)
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    if event.message.text == "info":
+        # Replace with the URL of your ESP32 endpoint
+        esp32_url = "http://esp32_ip_address/data"
+        
+        try:
+            response = requests.get(esp32_url)
+            data = response.json()
+            temperature = data["temperature"]
+            humidity = data["humidity"]
+            reply_message = f"Temperature: {temperature}°C, Humidity: {humidity}%"
+        except Exception as e:
+            reply_message = "Error fetching data from ESP32."
+
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=reply_message)
+        )
+
+if __name__ == "__main__":
+    app.run()
