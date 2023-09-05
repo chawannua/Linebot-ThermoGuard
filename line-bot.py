6ddd20ab54cc24b94cddf423581f6fdd91cd8cff
chawannua <55906160+chawannua@users.noreply.github.com>
2023-09-05 19:28:22 +0700

    test

--- line-bot.py
+++ line-bot.py
@@ -22,7 +22,7 @@ def callback():
     except InvalidSignatureError:
         abort(400)
     return 'OK'
-
+"""
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     if event.message.text == ("info","Info","INFO","ข้อมูล"):
@@ -43,6 +43,6 @@ def handle_message(event):
             event.reply_token,
             TextSendMessage(text=reply_message)
         )
-
+"""
 if __name__ == "__main__":
     app.run()
