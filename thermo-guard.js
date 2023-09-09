03e5f8b86e457c7b4b5c0b87da8867b7147d948b
chawannua <55906160+chawannua@users.noreply.github.com>
2023-09-10 03:47:59 +0700

    Big update

--- thermo-guard.js
+++ thermo-guard.js
@@ -1,13 +1,12 @@
-var express = require('express')
-var bodyParser = require('body-parser')
-var request = require('request')
-var app = express()
+var express = require('express');
+var bodyParser = require('body-parser');
+var request = require('request');
+var app = express();
 
 var mqtt = require('mqtt');
 
-// Your Channel access token (long-lived) 
+// Your Channel access token (long-lived)
 const CH_ACCESS_TOKEN = '7nntV9CadnWw54gO9B+lAJTF1Ap4RF5lCJatqOLRrzHZO0wrSewxnSh8bV9kJSHf0xuwIPW5gw+08gH3W3nVK6KuDW9AB6ctP5SxleybdphHk4klApt8z68dp2OXcliJ27pXppy4Un4cx7j8DTXraAdB04t89/1O/w1cDnyilFU=';
-
 
 // MQTT Host
 var mqtt_host = 'mqtt://m15.cloudmqtt.com';
@@ -17,270 +16,147 @@ var mqtt_topic = '/ESP32';
 
 // MQTT Config
 var options = {
-    port: 18772,
-    host: 'mqtt://m15.cloudmqtt.com',
-    clientId: 'mqttjs_' + Math.random().toString(16).substr(2, 8),
-
-    username: 'wfcsvmqa',
-    password: 'IqpnVbyPDHqi',
-    keepalive: 60,
-    reconnectPeriod: 1000,
-    protocolId: 'MQIsdp',
-    protocolVersion: 3,
-    clean: true,
-    encoding: 'utf8'
+  port: 18772,
+  host: 'mqtt://m15.cloudmqtt.com',
+  clientId: 'mqttjs_' + Math.random().toString(16).substr(2, 8),
+  username: 'wfcsvmqa',
+  password: 'IqpnVbyPDHqi',
+  keepalive: 60,
+  reconnectPeriod: 1000,
+  protocolId: 'MQIsdp',
+  protocolVersion: 3,
+  clean: true,
+  encoding: 'utf8',
 };
 
+app.use(bodyParser.json());
 
-app.use(bodyParser.json())
-
-app.set('port', (process.env.PORT || 4000))
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
+app.set('port', process.env.PORT || 4000);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.post('/webhook', (req, res) => {
-  var text = req.body.events[0].message.text.toLowerCase()
-  var sender = req.body.events[0].source.userId
-  var replyToken = req.body.events[0].replyToken
-  console.log(text, sender, replyToken)
-  console.log(typeof sender, typeof text)
-  // console.log(req.body.events[0])
+  var text = req.body.events[0].message.text.toLowerCase();
+  var sender = req.body.events[0].source.userId;
+  var replyToken = req.body.events[0].replyToken;
+  console.log(text, sender, replyToken);
+  console.log(typeof sender, typeof text);
 
-  if (text === 'info1' || text === 'Info1') {
-    // Info
-    inFo1(sender, text)
-  }
-  else if (text === 'info2' || text === 'Info2') {
-    // LED On
-   inFo2(sender, text)
-  }
-  else if (text === 'info3' || text === 'Info3') {
-    // LED Off
-    inFo3(sender, text)
-  }
-  else if (text === 'more data' || text === 'More data') {
-    data(sender, text)
+  if (text === 'data1' || text === 'Data1' || text === 'data2' || text === 'Data2' || text === 'data3' || text === 'Data3') {
+    // Determine the target ESP32 based on the received text
+    var espDevice;
+    if (text === 'data1' || text === 'Data1') {
+      espDevice = 'esp32_1';
+    } else if (text === 'data2' || text === 'Data2') {
+      espDevice = 'esp32_2';
+    } else {
+      espDevice = 'esp32_3';
+    }
+    // Send the corresponding command to the MQTT topic
+    var command = text === 'data1' || text === 'Data1' ? 'get_data' : text === 'data2' || text === 'Data2' ? 'on' : 'off';
+    sendMqttCommand(sender, espDevice, command);
+  } 
+  else if (text === 'website' || text === 'Website') {
+    // Help
+    sendText(sender, 'http://thermoguard.spaceac.net/');
   }
   else {
     // Other
-    sendText(sender, text);
+    sendText(sender, 'Please use the menu command or "data1," "data2," or "data3" command to control the ESP32 devices. For more info please visit http://thermoguard.spaceac.net/');
   }
 
-  res.sendStatus(200)
-}
-)
-function sendText (sender, text) {
+  res.sendStatus(200);
+});
+
+function sendText(sender, text) {
   let data = {
     to: sender,
     messages: [
       {
         type: 'text',
-        text: 'Please use only valid command for more info please visit http://thermoguard.spaceac.net/'
-      }
-    ]
-  }
-  request({
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer '+CH_ACCESS_TOKEN+''
+        text: text,
+      },
+    ],
+  };
+
+  request(
+    {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + CH_ACCESS_TOKEN,
+      },
+      url: 'https://api.line.me/v2/bot/message/push',
+      method: 'POST',
+      body: data,
+      json: true,
     },
-    url: 'https://api.line.me/v2/bot/message/push',
-    method: 'POST',
-    body: data,
-    json: true
-  }, function (err, res, body) {
-    if (err) console.log('error')
-    if (res) console.log('success')
-    if (body) console.log(body)
-  })
+    function (err, res, body) {
+      if (err) console.log('error');
+      if (res) console.log('success');
+      if (body) console.log(body);
+    }
+  );
 }
 
-function data (sender, text) {
-  let data = {
-    to: sender,
-    messages: [
-      {
-        type: 'text',
-        text: 'Here our website http://thermoguard.spaceac.net/'
-      }
-    ]
-  }
-  request({
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer '+CH_ACCESS_TOKEN+''
-    },
-    url: 'https://api.line.me/v2/bot/message/push',
-    method: 'POST',
-    body: data,
-    json: true
-  }, function (err, res, body) {
-    if (err) console.log('error')
-    if (res) console.log('success')
-    if (body) console.log(body)
-  })
-}
-
-function inFo1 (sender, text) {
-  let data = {
-    to: sender,
-    messages: [
-      {
-        type: 'text',
-        text: 'uid: '+sender
-      }
-    ]
-  }
-  request({
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer '+CH_ACCESS_TOKEN+''
-    },
-    url: 'https://api.line.me/v2/bot/message/push',
-    method: 'POST',
-    body: data,
-    json: true
-  }, function (err, res, body) {
-    if (err) console.log('error')
-    if (res) console.log('success')
-    if (body) console.log(body)
-  })
-}
-
-
-function inFo2 (sender, text) {
+function sendMqttCommand(sender, espDevice, command) {
+  // Create an MQTT client and connect to the broker
   var client = mqtt.connect(mqtt_host, options);
-  client.on('connect', function() { // When connected
-      console.log('MQTT connected');
-      // subscribe to a topic
-      client.subscribe(mqtt_topic, function() {
-          // when a message arrives, do something with it
-          client.on('message', function(topic, message, packet) {
-              console.log("Received '" + message + "' on '" + topic + "'");
-          });
-      });
-      
 
-      // publish a message to a topic
-      client.publish(mqtt_topic, 'on', function() {
-          console.log("Message is published");
-          client.end(); // Close the connection when published
-      });
-      
-  });
-    
-
-  let data = {
-    to: sender,
-    messages: [
-      {
-        type: 'text',
-        text: 'LED ON'
-      }
-    ]
-  }
-  request({
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer '+CH_ACCESS_TOKEN+''
-    },
-    url: 'https://api.line.me/v2/bot/message/push',
-    method: 'POST',
-    body: data,
-    json: true
-  }, function (err, res, body) {
-    if (err) console.log('error')
-    if (res) console.log('success')
-    if (body) console.log(body)
-  })
-}
-
-function inFo3 (sender, text) {
-  var client = mqtt.connect(mqtt_host, options);
-  client.on('connect', function() { // When connected
-      console.log('MQTT connected');
-      // subscribe to a topic
-      client.subscribe(mqtt_topic, function() {
-          // when a message arrives, do something with it
-          client.on('message', function(topic, message, packet) {
-              console.log("Received '" + message + "' on '" + topic + "'");
-          });
-      });
-      
-
-      // publish a message to a topic
-      client.publish(mqtt_topic, 'off', function() {
-          console.log("Message is published");
-          client.end(); // Close the connection when published
-      });
-      
-  });
-
-  let data = {
-    to: sender,
-    messages: [
-      {
-        type: 'text',
-        text: 'LED OFF'
-      }
-    ]
-  }
-  request({
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer '+CH_ACCESS_TOKEN+''
-    },
-    url: 'https://api.line.me/v2/bot/message/push',
-    method: 'POST',
-    body: data,
-    json: true
-  }, function (err, res, body) {
-    if (err) console.log('error')
-    if (res) console.log('success')
-    if (body) console.log(body)
-  })
-}
-
-function requestData(sender, text) {
-  // Publish a "get_data" command to the MQTT topic.
-  var client = mqtt.connect(mqtt_host, options);
+  // Subscribe to the MQTT topic where ESP32 sends responses
   client.on('connect', function () {
     console.log('MQTT connected');
-    // Publish a "get_data" command.
-    client.publish(mqtt_topic, 'get_data', function () {
-      console.log("Request for data sent");
-      client.end();
+    client.subscribe('/ESP32/response', function (err) {
+      if (!err) {
+        console.log('Subscribed to /ESP32/response');
+      }
     });
   });
 
-  // Send a message to the user indicating that data is being requested.
-  let data = {
-    to: sender,
-    messages: [
-      {
-        type: 'text',
-        text: 'Requesting data from ESP32...'
-      }
-    ]
-  };
+  // Handle incoming MQTT responses
+  client.on('message', function (topic, message) {
+    // Check if the topic is the one where the response is expected
+    if (topic === '/ESP32/response') {
+      // Send the received response as a message to the LINE user
+      let data = {
+        to: sender,
+        messages: [
+          {
+            type: 'text',
+            text: 'Received response from ' + espDevice + ': ' + message.toString(),
+          },
+        ],
+      };
 
-  request({
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer ' + CH_ACCESS_TOKEN
-    },
-    url: 'https://api.line.me/v2/bot/message/push',
-    method: 'POST',
-    body: data,
-    json: true
-  }, function (err, res, body) {
-    if (err) console.log('error');
-    if (res) console.log('success');
-    if (body) console.log(body);
+      // Send the message to the user via the LINE API
+      request(
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: 'Bearer ' + CH_ACCESS_TOKEN,
+          },
+          url: 'https://api.line.me/v2/bot/message/push',
+          method: 'POST',
+          body: data,
+          json: true,
+        },
+        function (err, res, body) {
+          if (err) console.log('error');
+          if (res) console.log('success');
+          if (body) console.log(body);
+        }
+      );
+
+      // Disconnect the MQTT client after receiving the response
+      client.end();
+    }
+  });
+
+  // Publish the command to the MQTT topic
+  client.publish(mqtt_topic + '/' + espDevice, command, function () {
+    console.log("Command sent to " + espDevice + ": " + command);
   });
 }
 
-
 app.listen(app.get('port'), function () {
-  console.log('run at port', app.get('port'))
-})
+  console.log('run at port', app.get('port'));
+});
