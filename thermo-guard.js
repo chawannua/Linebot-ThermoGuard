a8d429bf2c728f55a7e3756f3cca228e4e5e4182
chawannua <55906160+chawannua@users.noreply.github.com>
2023-09-12 17:57:08 +0700

    test again please work

--- thermo-guard.js
+++ thermo-guard.js
@@ -1,14 +1,12 @@
-//originla one
-
-var express = require('express')
-var bodyParser = require('body-parser')
-var request = require('request')
-var app = express()
+var express = require('express');
+var bodyParser = require('body-parser');
+var request = require('request');
+var app = express();
 
 var mqtt = require('mqtt');
 
-// Your Channel access token (long-lived) 
-const CH_ACCESS_TOKEN = '7nntV9CadnWw54gO9B+lAJTF1Ap4RF5lCJatqOLRrzHZO0wrSewxnSh8bV9kJSHf0xuwIPW5gw+08gH3W3nVK6KuDW9AB6ctP5SxleybdphHk4klApt8z68dp2OXcliJ27pXppy4Un4cx7j8DTXraAdB04t89/1O/w1cDnyilFU=';
+// Your Channel access token (long-lived)
+const CH_ACCESS_TOKEN = 'YOUR_LINE_CHANNEL_ACCESS_TOKEN';
 
 // MQTT Host
 var mqtt_host = 'mqtt://driver.cloudmqtt.com';
@@ -19,7 +17,6 @@ var mqtt_topic = '/ESP32';
 // MQTT Config
 var options = {
     port: 18772,
-    host: 'mqtt://driver.cloudmqtt.com',
     clientId: 'mqttjs_' + Math.random().toString(16).substr(2, 8),
     username: 'wfcsvmqa',
     password: 'IqpnVbyPDHqi',
@@ -31,216 +28,144 @@ var options = {
     encoding: 'utf8'
 };
 
+// Set up MQTT client
+var client = mqtt.connect(mqtt_host, options);
 
-app.use(bodyParser.json())
+// Handle MQTT client connection events
+client.on('connect', function() {
+    console.log('MQTT connected');
+    // Subscribe to the MQTT topic
+    client.subscribe(mqtt_topic, function(err) {
+        if (err) {
+            console.error('MQTT subscription failed:', err);
+        } else {
+            console.log('Subscribed to MQTT topic:', mqtt_topic);
+        }
+    });
+});
 
-app.set('port', (process.env.PORT || 4000))
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
+client.on('message', function(topic, message) {
+    console.log("Received '" + message.toString() + "' on '" + topic + "'");
+    // Handle incoming MQTT messages here
+    // You can send Line messages here as well
+    // Example:
+    sendLineMessage(sender, message.toString());
+});
 
-app.post('/webhook', (req, res) => {
-  var text = req.body.events[0].message.text.toLowerCase()
-  var sender = req.body.events[0].source.userId
-  var replyToken = req.body.events[0].replyToken
-  console.log(text, sender, replyToken)
-  console.log(typeof sender, typeof text)
-  // console.log(req.body.events[0])
+// Middleware to parse JSON requests
+app.use(bodyParser.json());
 
-  if (text === 'info' || text === 'รายงาน') {
-    // Info
-    inFo(sender, text)
+// Set the port for the Express.js app
+app.set('port', (process.env.PORT || 4000));
+
+// Handle incoming Line messages
+app.post('/webhook', function(req, res) {
+    var text = req.body.events[0].message.text.toLowerCase();
+    var sender = req.body.events[0].source.userId;
+    var replyToken = req.body.events[0].replyToken;
     
-  }
-  else if (text === '1' || text === 'เปิด' || text === 'on') {
-    // LED On
-    ledOn(sender, text)
-    init(sender, text)
-  }
-  else if (text === '0' || text === 'ปิด' || text === 'off') {
-    // LED Off
-    ledOff(sender, text)
-    init(sender, text)
-  }
-  else {
-    // Other
-    sendText(sender, text);
-  }
+    console.log(text, sender, replyToken);
+    console.log(typeof sender, typeof text);
 
-  res.sendStatus(200)
-})
+    if (text === 'info' || text === 'รายงาน') {
+        // Info
+        inFo(sender, text);
+    } else if (text === '1' || text === 'เปิด' || text === 'on') {
+        // LED On
+        ledOn(sender, text);
+    } else if (text === '0' || text === 'ปิด' || text === 'off') {
+        // LED Off
+        ledOff(sender, text);
+    } else {
+        // Other
+        sendText(sender, text);
+    }
 
-function sendText (sender, text) {
-  let data = {
-    to: sender,
-    messages: [
-      {
-        type: 'text',
-        text: 'กรุณาพิมพ์ : info | on | off | เปิด | ปิด เท่านั้น'
-      }
-    ]
-  }
-  request({
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer '+CH_ACCESS_TOKEN+''
-    },
-    url: 'https://api.line.me/v2/bot/message/push',
-    method: 'POST',
-    body: data,
-    json: true
-  }, function (err, res, body) {
-    if (err) console.log('error')
-    if (res) console.log('success')
-    if (body) console.log(body)
-  })
-}
+    res.sendStatus(200);
+});
 
-function inFo (sender, text) {
-  let data = {
-    to: sender,
-    messages: [
-      {
-        type: 'text',
-        text: 'uid: '+sender
-      }
-    ]
-  }
-  request({
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer '+CH_ACCESS_TOKEN+''
-    },
-    url: 'https://api.line.me/v2/bot/message/push',
-    method: 'POST',
-    body: data,
-    json: true
-  }, function (err, res, body) {
-    if (err) console.log('error')
-    if (res) console.log('success')
-    if (body) console.log(body)
-  })
-}
-
-
-function ledOn (sender, text) {
-  var client = mqtt.connect(mqtt_host, options);
-  client.on('connect', function() { // When connected
-      console.log('MQTT connected');
-      // subscribe to a topic
-      client.subscribe(mqtt_topic, function() {
-          // when a message arrives, do something with it
-          client.on('message', function(topic, message, packet) {
-              console.log("Received '" + message + "' on '" + topic + "'");
-          });
-      });
-      
-
-      // publish a message to a topic
-      client.publish(mqtt_topic, 'on', function() {
-          console.log("Message is published");
-          client.end(); // Close the connection when published
-      });
-      
-  });
+function sendText(sender, text) {
+    let data = {
+        to: sender,
+        messages: [
+            {
+                type: 'text',
+                text: 'กรุณาพิมพ์ : info | on | off | เปิด | ปิด เท่านั้น'
+            }
+        ]
+    };
     
-
-  let data = {
-    to: sender,
-    messages: [
-      {
-        type: 'text',
-        text: 'LED ON'
-      }
-    ]
-  }
-  request({
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer '+CH_ACCESS_TOKEN+''
-    },
-    url: 'https://api.line.me/v2/bot/message/push',
-    method: 'POST',
-    body: data,
-    json: true
-  }, function (err, res, body) {
-    if (err) console.log('error')
-    if (res) console.log('success')
-    if (body) console.log(body)
-  })
+    sendLineMessage(data);
 }
 
-function init() {
-  // Set up MQTT client
-  var client = mqtt.connect(mqtt_host, options);
-  client.on('connect', function() {
-      console.log('MQTT connected');
-      // subscribe to a topic
-      client.subscribe(mqtt_topic, function() {
-          // when a message arrives, do something with it
-          client.on('message', function(topic, message, packet) {
-              console.log("Received '" + message + "' on '" + topic + "'");
-              // Handle incoming MQTT messages here
-              // You can send Line messages here as well
-              let data = {
-                to: sender,
-                messages: [
-                  {
-                    type: 'text',
-                    text: message
-                  }
-                ]
-              }
-          });
-      });
-  });
-
-function ledOff (sender, text) {
-  var client = mqtt.connect(mqtt_host, options);
-  client.on('connect', function() { // When connected
-      console.log('MQTT connected');
-      // subscribe to a topic
-      client.subscribe(mqtt_topic, function() {
-          // when a message arrives, do something with it
-          client.on('message', function(topic, message, packet) {
-              console.log("Received '" + message + "' on '" + topic + "'");
-          });
-      });
-      
-
-      // publish a message to a topic
-      client.publish(mqtt_topic, 'off', function() {
-          console.log("Message is published");
-          client.end(); // Close the connection when published
-      });
-      
-  });
-
-  let data = {
-    to: sender,
-    messages: [
-      {
-        type: 'text',
-        text: 'LED OFF'
-      }
-    ]
-  }
-  request({
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer '+CH_ACCESS_TOKEN+''
-    },
-    url: 'https://api.line.me/v2/bot/message/push',
-    method: 'POST',
-    body: data,
-    json: true
-  }, function (err, res, body) {
-    if (err) console.log('error')
-    if (res) console.log('success')
-    if (body) console.log(body)
-  })
+function inFo(sender, text) {
+    let data = {
+        to: sender,
+        messages: [
+            {
+                type: 'text',
+                text: 'uid: ' + sender
+            }
+        ]
+    };
+    
+    sendLineMessage(data);
 }
 
+function ledOn(sender, text) {
+    // Publish an MQTT message to turn on the LED
+    client.publish(mqtt_topic, 'on');
+    
+    let data = {
+        to: sender,
+        messages: [
+            {
+                type: 'text',
+                text: 'LED ON'
+            }
+        ]
+    };
+    
+    sendLineMessage(data);
+}
+
+function ledOff(sender, text) {
+    // Publish an MQTT message to turn off the LED
+    client.publish(mqtt_topic, 'off');
+    
+    let data = {
+        to: sender,
+        messages: [
+            {
+                type: 'text',
+                text: 'LED OFF'
+            }
+        ]
+    };
+    
+    sendLineMessage(data);
+}
+
+function sendLineMessage(messageData) {
+    request({
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + CH_ACCESS_TOKEN
+        },
+        url: 'https://api.line.me/v2/bot/message/push',
+        method: 'POST',
+        body: messageData,
+        json: true
+    }, function (err, res, body) {
+        if (err) {
+            console.error('Error sending Line message:', err);
+        } else {
+            console.log('Line message sent:', body);
+        }
+    });
+}
+
+// Start the Express.js server
 app.listen(app.get('port'), function () {
-  console.log('run at port', app.get('port'))
-})
-
+    console.log('App is running on port', app.get('port'));
+});
